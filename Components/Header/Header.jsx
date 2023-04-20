11419218e687d646335c9c4d2daf361646230540
Ankan112 <ankanhalder112@gmail.com>
2023-04-21 01:30:43 +0600

    banner background added

--- Components/Header/Header.jsx
+++ Components/Header/Header.jsx
@@ -2,32 +2,32 @@ import Link from "next/link";
 
 const Header = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar text-white bg-[#190245] bg-opacity-80 fixed">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-[#190245] rounded-box w-52">
                         <li><Link href={'/'}>Home</Link></li>
                         <li><Link href={'/march'}>March</Link></li>
                         <li><Link href={'/shows'}>Shows</Link></li>
                         <li><Link href={'/extras'}>Extras</Link></li>
                     </ul>
                 </div>
-                <Link href={'/'} className="btn btn-ghost normal-case text-xl">Kimbhoot</Link>
+                <Link href={'/'} className="btn btn-ghost normal-case text-xl">কিম্ভূত</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><Link href={'/'}>Home</Link></li>
+                    <li><Link href={'/'}>হোম</Link></li>
                     <li><Link href={'/march'}>March</Link></li>
                     <li><Link href={'/shows'}>Shows</Link></li>
                     <li><Link href={'/extras'}>Extras</Link></li>
                 </ul>
             </div>
             <div className="navbar-end">
-                <Link href={'/content'} className="btn mr-5">Content</Link>
-                <Link href={'/team'} className="btn">Team</Link>
+                <Link href={'/content'} className="mr-5">কনটেন্ট</Link>
+                <Link href={'/team'} className=" mr-5">Team</Link>
             </div>
         </div>
     );
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,11 @@ import Header from '@/Components/Header/Header'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />)
+  }
+
   return (
     <>
       <Header></Header>
